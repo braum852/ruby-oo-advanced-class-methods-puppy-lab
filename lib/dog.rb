38c0bd479450ccd6be453ff5177cadcfac68b0f8
braum852 <julian.teo59@yahoo.com>
2022-02-07 18:47:01 +1000

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+class Dog
+    attr_reader :name
+
+@@all = []
+
+def initialize(name)
+    @name = name
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.clear_all
+    @@all.clear
+    ##.clear removes all element in array
+end
+
+def self.print_all   
+    puts @@all.map {|x| x.name}
+    ##prints all dogs names
+end
+
+def save
+    @@all << self
+    ## adds dog instance to @@all array when called
+    ##gets called inside initialize when dog created
+end
+
+end
